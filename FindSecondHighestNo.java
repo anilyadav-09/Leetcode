3a05071638e0ec12523e52ca3caf15a48464be1d
AnilKumar Yadav <anilknby97@gmail.com>
2025-09-04 14:14:11 +0530

    Update FindSecondHighestNo.java

--- FindSecondHighestNo.java
+++ FindSecondHighestNo.java
@@ -1,20 +1,20 @@
 package org.example.java8;
 import java.util.*;
-/*Given an array of positive integers arr[], return the second largest element from the array. If the second-largest element doesn't exist, then return -1.
+/*Given an array of positive integers arr[], return the second-largest element from the array. If the second-largest element doesn't exist, then return -1.
 Note: The second largest element should not be equal to the largest element.
 
 Examples:
 Input: arr[] = [12, 35, 1, 10, 34, 1]
 Output: 34
-Explanation: The largest element of the array is 35 and the second largest element is 34.
+Explanation: The largest element of the array is 35, and the second-largest element is 34.
 
 Input: arr[] = [10, 5, 10]
 Output: 5
-Explanation: The largest element of the array is 10, and the second largest element is 5.
+Explanation: The largest element of the array is 10, and the second-largest element is 5.
 
 Input: arr[] = [10, 10, 10]
 Output: -1
-Explanation: The largest element of the array is 10, and the second largest element does not exist.*/
+Explanation: The largest element of the array is 10, and the second-largest element does not exist.*/
 
 public class FindSecondHighestNo {
     public static void main(String[] args) {
